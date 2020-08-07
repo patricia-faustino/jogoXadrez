b669bd3fccd015f3aa403432213a0b06144f77c7
Patricia Faustino <patriciafaustinobr@gmail.com>
2020-08-07 12:20:56 -0300

    Tratando erros com excecao personalizada

--- xadrez1/Program.cs
+++ xadrez1/Program.cs
@@ -8,15 +8,20 @@ namespace xadrez1
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            Console.WriteLine();
-                
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                Console.WriteLine();
 
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 9));
 
+                Tela.imprimirTabuleiro(tab);
+            }catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- xadrez1/tabuleiro/Tabuleiro.cs
+++ xadrez1/tabuleiro/Tabuleiro.cs
@@ -17,19 +17,57 @@ namespace tabuleiro
             pecas = new Peca[linhas, colunas];
         }
 
+        
         //permicao para programador acessar peca individual já que a matriz peca é private
         public Peca peca(int linhas, int colunas)
         {
             return pecas[linhas, colunas];
         }
 
+        //Sobrecarga para receber pela matriz pela posicao indicada
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linhas, pos.colunas];
+        }
+        //Metodo para verificar se existe uma posicao e se ela eh valida
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos)!=null;
+        }
+
+
         //Colocar peca p na posicao pos
         public void colocarPeca(Peca p, Posicao pos)
         {
+            //soh pode colocar peca onde nao tem outra peca
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição.");
+            }
             // ir na matriz na posicao pecas e posicao coluna  e receber p
             pecas[pos.linhas, pos.colunas] = p;
             // ir na peca p e dizer que a peca p vai ter uma posicao pos
             p.posicao = pos;
+        }
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linhas < 0 || pos.linhas >= linhas || pos.colunas < 0 || pos.colunas >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Retornar excecao personalizada caso a posicao nao seja valida
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+
+
         }
     }
 }
