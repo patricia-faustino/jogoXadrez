f26dcef3460589b241d59bb1636bf875e07650a6
Patricia Faustino <patriciafaustinobr@gmail.com>
2020-08-07 11:17:08 -0300

    Criando a classe posicao

--- xadrez1/Program.cs
+++ xadrez1/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using tabuleiro;
 namespace xadrez1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posição: "+p);
             Console.WriteLine("Hello World!");
         }
     }
---
+++ xadrez1/tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        public Posicao(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+        }
+        public override string ToString()
+        {
+            return linhas+
+                " , "+
+                colunas;
+        }
+    }
+}
